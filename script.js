078f34b874fc5c355a69c4e48e0678d2fabee096
Vladimir RM <shvdrummer@gmail.com>
2022-11-29 09:33:44 +0200

    function createDeleteElements

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ const btn = document.querySelector("#btn")
 const result = document.querySelector("#result")
 const total = document.querySelector("#total")
 
-btn.addEventListener('click')
+btn.addEventListener('click', (value)=>{
+  console.log(input.value);
+  input.value = ''
+})
+
+
+
+function createDeleteElements (){
+  
+}
