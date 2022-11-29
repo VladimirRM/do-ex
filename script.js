d9d274a01946a98799281b5c63dd2346b531b080
Vladimir RM <shvdrummer@gmail.com>
2022-11-29 10:20:26 +0200

    check input value

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-const input = document.querySelector("#input")
-const btn = document.querySelector("#btn")
-const result = document.querySelector("#result")
-const total = document.querySelector("#total")
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+const result = document.querySelector("#result");
+const total = document.querySelector("#total");
 
-btn.addEventListener('click', (value)=>{
+btn.addEventListener("click", (e) => {
   console.log(input.value);
-  if(input.value === '')return
-  createDeleteElements (input.value)
-  input.value = ''
-})
-
-
-
-function createDeleteElements (){
-   const li
-
-}
+});
