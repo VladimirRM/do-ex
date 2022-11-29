d5f71aa3ffde4399ea00f54e59bc7f1fbfec10f4
Vladimir RM <shvdrummer@gmail.com>
2022-11-29 17:10:19 +0200

    create let i

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const input = document.querySelector("#input");
 const btn = document.querySelector("#btn");
 const result = document.querySelector("#result");
 const total = document.querySelector("#total");
+let i = 0;
 
 // get input value
 
