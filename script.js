0ff7c642ba6f350435c68e47255a2a89401655e6
Vladimir RM <shvdrummer@gmail.com>
2022-12-09 10:05:29 +0200

    clear code

--- script.js
+++ script.js
@@ -4,29 +4,3 @@ const result = document.querySelector("#result");
 const total = document.querySelector("#total");
 let i = 0;
 
-// get input value
-
-btn.addEventListener("click", (e) => {
-  if (input.value === "") return;
-  createDeleteElements(input.value);
-  input.value = "";
-});
-
-function createDeleteElements(value) {
-  const li = document.createElement("li");
-  li.className = "li";
-  li.textContent = value;
-  const btn = document.createElement("button");
-  btn.className = "btn";
-  btn.textContent = "delete";
-  li.appendChild(btn);
-
-  btn.addEventListener("click", (e) => {
-    result.removeChild(li);
-  });
-  li.addEventListener("click", (e) => {
-    li.classList.toggle("li-active");
-  });
-
-  result.appendChild(li);
-}
