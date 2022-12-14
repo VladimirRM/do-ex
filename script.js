704adf31176f02edecb7cd2cd9c4eac573bd5b39
Vladimir RM <shvdrummer@gmail.com>
2022-12-14 14:51:16 +0200

    create button for input

--- script.js
+++ script.js
@@ -4,33 +4,8 @@ const result = document.querySelector("#result");
 const total = document.querySelector("#total");
 let i = 0;
 
-btn.addEventListener("click", (e) => {
-  i++;
-  if (input.value === "") return;
-  createDeleteElements(input.value);
-  input.value = "";
-});
-
-function createDeleteElements(value) {
-  const li = document.createElement("li");
-  li.className = "li";
-  li.textContent = value;
-  const btn = document.createElement("button");
-  btn.className = "btn";
-  btn.textContent = "Delete";
-  li.appendChild(btn);
-
-  btn.addEventListener("click", (e) => {
-    i--;
-    total.textContent = i;
-
-    result.removeChild(li);
-  });
-  li.addEventListener("click", (e) => {
-    li.classList.toggle("li-active");
-  });
-
-  total.textContent = i;
-
-  result.appendChild(li);
-}
+btn.addEventListener('click',(e)=>{
+  if (input.value==='')return
+  createDeleteElements(input.value)
+  input.value = ''
+})
